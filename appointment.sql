f6bf8e1f320cc58ba7a932dcb73d278b0cc919b1
Thanikul Sriuthis <moremeng@dv4.biz>
2019-09-10 12:02:24 +0700

    fix date, time name lowercase

--- appointment.sql
+++ appointment.sql
@@ -1,11 +1,9 @@
-CREATE VIEW [dbo].[h4u_appointment]
-AS
 SELECT ltrim(o.hn) AS hn,
 	o.regNo AS seq,
 	convert(DATE, convert(CHAR, o.registDate - 5430000)) AS date_serve,
 	convert(TIME, left(o.timePt, 2) + ':' + right(o.timePt, 2)) AS time_serve,
-	RTRIM(CONVERT(CHAR, CONVERT(BIGINT, A.appoint_date) - 5430000)) AS DATE,
-	convert(TIME, left(A.appoint_time_from, 2) + ':' + right(A.appoint_time_from, 2)) AS TIME,
+	RTRIM(CONVERT(CHAR, CONVERT(BIGINT, A.appoint_date) - 5430000)) AS date,
+	convert(TIME, left(A.appoint_time_from, 2) + ':' + right(A.appoint_time_from, 2)) AS time,
 	d.deptDesc AS department,
 	A.appoint_note AS detail
 FROM dbo.OPD_H(NOLOCK) AS o
